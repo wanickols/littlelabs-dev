9e4ae7a653459123e1071a3479823921c6b17b2e
wanickols01 <wanickols@ualr.edu>
2025-07-11 14:51:50 -0500

    Tailwind 4.1 setup
    Added tailwind
    Added a space font

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Little Labs</h1>
+  <div class="min-h-screen flex items-center justify-center bg-slate-900">
+    <h1 class="text-5x1 text-white">
+      Little Labs
+    </h1>
+  </div>
 </template>
+
