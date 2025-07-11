06e7d6e64af3aff4871d1b3ce2dc94127e35d7a5
wanickols01 <wanickols@ualr.edu>
2025-07-11 15:10:10 -0500

    Prettier plugin add
    was annoying me it wouldn't autoformat

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-slate-900">
-    <h1 class="text-5x1 text-white">
-      Little Labs
-    </h1>
+    <h1 class="text-5x1 text-white">Little Labs</h1>
   </div>
 </template>
-
