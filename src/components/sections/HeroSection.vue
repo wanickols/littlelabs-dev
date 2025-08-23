3d50a1b56aa6bf8c012d11f03d2892b5dc92e28b
wanickols01 <wanickols@ualr.edu>
2025-08-22 22:17:43 -0500

    Hero Update
    just changing text

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -4,12 +4,13 @@
   >
     <!-- Text Column -->
     <div class="max-w-xl text-center md:text-left">
-      <h1 class="titlexl">Small Tools. Clear Purpose.</h1>
+      <h1 class="titlexl">Small Tools. Real Purpose.</h1>
       <p class="paragraph mt-4">
-        I build modular, thoughtful software — from quiet utility apps to games
-        with a heartbeat. Every part has a purpose. Every project is made to
-        last.
+        I make software that matters to me — simple apps and games that do what
+        I hope they will. They may be small, they may not change the world, but
+        they are built to last.
       </p>
+
       <div class="mt-6 flex flex-wrap justify-center md:justify-start gap-4">
         <RouterLink to="/projects" class="btn btn-primary"
           >View Projects</RouterLink
